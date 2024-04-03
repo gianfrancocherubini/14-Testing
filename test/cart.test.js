0dd42f2fcb1b9d373396789ead11d00a457d66a4
gianfranco cherubini <cherussa@gmail.com>
2024-04-03 20:45:55 -0300

    listo

--- test/cart.test.js
+++ test/cart.test.js
@@ -28,16 +28,16 @@ describe('PRUEBA MODULO DE CARTS', function () {
     describe('Prueba Router carrito', function () {
 
         it('Prueba POST. Debe retornar un status 201 al crear un carrito (/api/carts)', async function () {
-            const response = await requester.post('/api/carts').send(); // Realiza una solicitud POST al endpoint
+            const response = await requester.post('/api/carts').send(); 
             expect(response.status).to.equal(201); 
         });
         it('Prueba GET, renderizado de carrito por id. (/carrito/:cid)=> Debe renderizar el carrito por id y responder con status 200', async function () {
-            const cartId = '65e1f9f3488e3ae8149999fd';
+            const cartId = '660de84a959c6f10e0f0147c';
             const response = await requester.get(`/carrito/${cartId}`);
             expect(response.status).to.equal(200);
         });
         it('Prueba DELETE, eliminar carrito por id. (/c/:cid)=> Debe eliminar el carrito por id y responder con status 200', async function () {
-            const cartId = '65e1f9f3488e3ae8149999fd';
+            const cartId = '660de84a959c6f10e0f0147c7';
             const response = await requester.delete(`/api/carts/${cartId}`);
             expect(response.status).to.equal(200);
         });
